79f078637988efe12b48f5e323a53b6aea09e740
AllaBeloshapka <allabeloshapka4@gmail.com>
2025-11-17 10:21:17 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 import Lesson_02 from "./lessons/lasson_02/lesson_02";
 import Homework_02 from "./homeworks/hw_02/Homework_02";
 import LoginForm from "./homeworks/Hw_03/LoginForm/LoginForm";
-import Homework_03 from "./homeworks/hw_03/Homework_03";
+import Homework_03  from "./homeworks/hw_03/Homework_03";
 
 function App() {
   return (
