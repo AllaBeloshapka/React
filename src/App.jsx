01d4b1a3263cc3be2a48de823b65d076e4ef9922
AllaBeloshapka <allabeloshapka4@gmail.com>
2025-11-20 13:52:45 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@
 
 import Lesson_02 from "./lessons/lasson_02/lesson_02";
 import Homework_02 from "./homeworks/hw_02/Homework_02";
-
 import Homework_03 from "./homeworks/hw_03/Homework_03"
+import Lesson_04 from "./lessons/lesson_04/Lesson_04";
+import Homework_04 from "./homeworks/Homework_04"
 
 function App() {
   return (
     <>
       {/* <Lesson_02 /> */}
       {/* <Homework_02/> */}
-      <Homework_03/>
+      {/* <Homework_03/> */}
+      {/* <Lesson_04/> */}
+      <Homework_04 />
       
     </>
   );
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,14 +1,40 @@
-import Button from "../Button/Button";
+// 1 ШАГ: Импортируем useState из библиотеки react
+import { useState } from "react";
+import Button from "./Button/Button";
 import "./styles.css";
 function Counter() {
-  const onMinus = () => {};
-  const onPlus = () => {};
+  // При старом способе компонент Counter не перезагружается и не может показать на странице актуальное значения переменных
+  // let count = 0;
+  // console.log("В теле компонента Counter", count);
+  // const onMinus = () => {
+  //   count = count - 1;
+  //   console.log("onMinus function", count);
+  // };
+  // const onPlus = () => {
+  //   count = count + 1;
+  //   console.log("onPlus function", count);
+  // };
+
+  const [count, setCount] = useState(0);  // аргумент по умолчанию
+  
+
+  const onPlus = () => {
+    setCount((prevVelue)=>{
+    return prevVelue + 1;
+    })
+  };
+  const onMinus = () => {
+        setCount((prevVelue)=>{
+    return prevVelue - 1;
+    })
+  };
+
   return (
     <div className="counter_wrapper">
       <div className="button_control">
         <Button name="-" onClick={onMinus} />
       </div>
-      <p className="count">{0}</p>
+      <p className="count">{count}</p>
       <div className="button_control">
         <Button name="+" onClick={onPlus} />
       </div>
